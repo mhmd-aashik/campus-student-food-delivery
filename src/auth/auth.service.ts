f2126d90d56025cbee492a123d9a6d7bee3f31da
Mohammed Aashik <aashikdevelop@gmail.com>
2026-06-10 23:28:24 +0530

    feat: implement user login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,20 @@
 import { UsersService } from '@/users/users.service';
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { RegisterDto } from './dto/register.dto';
 import * as bcrypt from 'bcryptjs';
+import { ConfigService } from '@nestjs/config';
+import { LoginDto } from './dto/login.dto';
 @Injectable()
 export class AuthService {
   constructor(
     protected readonly usersService: UsersService,
     protected readonly jwtService: JwtService,
+    protected readonly configService: ConfigService,
   ) {}
 
   async register(registerDto: RegisterDto) {
@@ -25,5 +32,58 @@ export class AuthService {
       passwordHash,
       role: registerDto.role,
     });
+  }
+
+  async login(loginDto: LoginDto) {
+    const user = await this.usersService.findByEmail(loginDto.email);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      loginDto.password,
+      user.passwordHash,
+    );
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const tokens = await this.generateTokens(user.id, user.email, user.role);
+    const hashRefreshToken = await bcrypt.hash(tokens.refreshToken, 10);
+    await this.usersService.updateRefreshToken(user.id, hashRefreshToken);
+
+    return {
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      ...tokens,
+    };
+  }
+
+  async generateTokens(userId: string, email: string, role: string) {
+    const payload = { sub: userId, email, role };
+    const refreshPayload = { sub: userId };
+
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(payload, {
+        secret: this.configService.getOrThrow<string>('JWT_SECRET'),
+        expiresIn: '15m',
+      }),
+
+      this.jwtService.signAsync(refreshPayload, {
+        secret: this.configService.getOrThrow<string>('JWT_REFRESH_SECRET'),
+        expiresIn: '7d',
+      }),
+    ]);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
   }
 }
