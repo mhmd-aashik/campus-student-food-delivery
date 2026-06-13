9d804846b76cdb6eb0f146f9f104a86075e2992d
Mohammed Aashik <aashikdevelop@gmail.com>
2026-06-13 17:46:53 +0530

    fix: correct variable name typos in RolesGuard and enhance UsersController with role-based access control

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -2,21 +2,23 @@ import {
   CanActivate,
   ExecutionContext,
   ForbiddenException,
+  Injectable,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Role } from '../enums/role.enum';
 import { ROLES_KEY } from '../decorators/roles.decorator';
 
+@Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const reqiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
 
-    if (!reqiredRoles || reqiredRoles.length === 0) {
+    if (!requiredRoles || requiredRoles.length === 0) {
       return true;
     }
 
@@ -30,7 +32,7 @@ export class RolesGuard implements CanActivate {
       throw new ForbiddenException('Access denied : User role not found');
     }
 
-    const hasRole = reqiredRoles.includes(user.role);
+    const hasRole = requiredRoles.includes(user.role);
 
     if (!hasRole) {
       throw new ForbiddenException('Access denied : Insufficient permissions');
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,7 @@
+import { Roles } from '@/auth/decorators/roles.decorator';
+import { Role } from '@/auth/enums/role.enum';
 import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
+import { RolesGuard } from '@/auth/guards/roles.guard';
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 
@@ -6,13 +9,15 @@ interface RequestWithUser extends Request {
   user?: {
     id: string;
     email: string;
-    role: string;
+    role: Role;
   };
 }
 
 @Controller('users')
 export class UsersController {
   @Get('me')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.CUSTOMER, Role.RESTAURANT, Role.DRIVER)
   @UseGuards(JwtAuthGuard)
   getMe(@Req() req: RequestWithUser) {
     return req.user;
