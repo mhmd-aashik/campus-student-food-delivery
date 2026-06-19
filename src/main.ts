2e2441ef939121d192587aa26e3540987b19819a
Mohammed Aashik <aashikdevelop@gmail.com>
2026-06-19 08:39:38 +0530

    feat: complete api documentation

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 import { TransformResponseInterceptor } from './common/interceptors/transform-response.interceptor';
 import { StructuredLogger } from './common/logger/structured-logger.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, {
@@ -29,6 +30,16 @@ async function bootstrap(): Promise<void> {
 
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new TransformResponseInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('Campus Food Delivery API')
+    .setDescription('The Campus Food Delivery platform API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3005);
 }
 void bootstrap();
