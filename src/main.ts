87d81311fd03ec011fe1e7c27d55dd45b164000a
Mohammed Aashik <aashikdevelop@gmail.com>
2026-06-19 05:56:13 +0530

    feat: add request validation

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,11 @@ async function bootstrap(): Promise<void> {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      transform: true,
       forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     }),
   );
 
